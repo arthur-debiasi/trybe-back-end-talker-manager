28378c2fff3044df2ae955f798ce59295940dfd0
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-14 17:16:55 -0300

    feat: R1 GET /talker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const TALKER_PATH = './src/talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (request, response) => {
+  const data = await fs.readFile(TALKER_PATH, 'utf-8');
+  response.status(HTTP_OK_STATUS).json(JSON.parse(data)); 
 });
 
 app.listen(PORT, () => {
