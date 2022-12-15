dc7863b9a8c6df57eec72459ab6796d23d0495e4
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-15 10:28:44 -0300

    feat: watchedAt validation mid

---
+++ src/middlewares/watchedAtValidation.js
@@ -0,0 +1,17 @@
+const dateValidFormat = require('../utils/dateValidFormat');
+const { HTTP_BAD_REQUEST_STATUS } = require('../utils/httpStatus/httpStatus');
+
+const watchedAtValidation = (request, response, next) => {
+  const { talk: { watchedAt } } = request.body;
+  if (!watchedAt) {
+    return response.status(HTTP_BAD_REQUEST_STATUS)
+    .json({ message: 'O campo watchedAt é obrigatório' });
+  }
+  if (!dateValidFormat(watchedAt)) {
+    return response.status(HTTP_BAD_REQUEST_STATUS)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+module.exports = watchedAtValidation;
