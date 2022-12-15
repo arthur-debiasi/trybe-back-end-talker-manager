c98dafaf0a6ed241507637aa85c64cb44c0b60eb
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-15 10:00:17 -0300

    feat: nameValidation

--- src/middlewares/nameValidation.js
+++ src/middlewares/nameValidation.js
@@ -1,6 +1,18 @@
+const { HTTP_BAD_REQUEST_STATUS } = require('../utils/httpStatus/httpStatus');
+
 const nameValidation = (request, response, next) => {
   const { name } = request.body;
-  if (name.length < 3) {
-     return response.status(HTTP)
+  if (!name) {
+    return response
+    .status(HTTP_BAD_REQUEST_STATUS)
+    .json({ message: 'O campo "name" é obrigatório' });
   }
+  if (name.length < 3) {
+     return response
+     .status(HTTP_BAD_REQUEST_STATUS)
+     .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
 };
+
+module.exports = nameValidation;
